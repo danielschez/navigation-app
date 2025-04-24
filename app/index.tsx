610ef027eb39156f2e35f92157d8880ce0a52102
daniel-hernandezs <dhsanchez@cerrajes.com>
2025-04-24 16:35:41 -0600

    Configuración de NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <View className='mt-10'>
-      <Text className='text-3xl font-bold text-blue-500'>Hola MUNDO!!!</Text>
+      <Text className='text-3xl font-bold text-blue-500'>App</Text>
     </View>
   )
 }
